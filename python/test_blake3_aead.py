1711650fe0eb8abcf9149d6b5610847ed25426f0
Jack O'Connor <oconnor663@gmail.com>
2023-07-03 13:28:29 -0700

    encrypt_b3sum for testing

--- python/test_blake3_aead.py
+++ python/test_blake3_aead.py
@@ -6,17 +6,23 @@ from blake3_aead import (
     xor,
     BLOCK_LEN,
     TAG_LEN,
+    MAX_MSG_LEN,
+    MAX_NONCE_LEN,
+    MSG_AUTH_SEEK,
+    AAD_AUTH_SEEK,
+    STREAM_SEEK,
 )
 from tempfile import NamedTemporaryFile
 import secrets
 import subprocess
 
 
-def universal_hash_cli(
-    one_time_key: bytes,
+def universal_hash_b3sum(
+    key: bytes,
     message: bytes,
     initial_seek: int,
 ) -> bytes:
+    assert initial_seek % BLOCK_LEN == 0
     result = bytes(TAG_LEN)
     for i in range(0, len(message), BLOCK_LEN):
         with NamedTemporaryFile() as f:
@@ -34,19 +40,63 @@ def universal_hash_cli(
                     "--seek",
                     str(initial_seek + i),
                 ],
-                input=one_time_key,
+                input=key,
                 stdout=subprocess.PIPE,
             ).stdout
             result = xor(result, block_output)
     return result
 
 
-def test_implementations_agree() -> None:
+def test_universal_hash_implementations_agree() -> None:
     for length in [0, 1, 64, 65, 128, 1000]:
         key = secrets.token_bytes(blake3.key_size)
         message = secrets.token_bytes(length)
         regular_result = universal_hash(key, message, 99 * BLOCK_LEN)
-        cli_result = universal_hash_cli(key, message, 99 * BLOCK_LEN)
+        cli_result = universal_hash_b3sum(key, message, 99 * BLOCK_LEN)
+        assert regular_result == cli_result, f"length {length}"
+
+
+def encrypt_b3sum(
+    key: bytes,
+    nonce: bytes,
+    aad: bytes,
+    plaintext: bytes,
+) -> bytes:
+    assert len(nonce) <= MAX_NONCE_LEN
+    assert len(aad) <= MAX_MSG_LEN
+    assert len(plaintext) <= MAX_MSG_LEN
+    with NamedTemporaryFile() as f:
+        f.write(nonce)
+        f.flush()
+        stream = subprocess.run(
+            [
+                "b3sum",
+                f.name,
+                "--keyed",
+                "--raw",
+                "--length",
+                str(len(plaintext) + TAG_LEN),
+                "--seek",
+                str(STREAM_SEEK),
+            ],
+            input=key,
+            stdout=subprocess.PIPE,
+        ).stdout
+    ciphertext_msg = xor(plaintext, stream[: len(plaintext)])
+    msg_tag = universal_hash_b3sum(key, ciphertext_msg, MSG_AUTH_SEEK)
+    aad_tag = universal_hash_b3sum(key, aad, AAD_AUTH_SEEK)
+    tag = xor(stream[len(plaintext) :], xor(msg_tag, aad_tag))
+    return ciphertext_msg + tag
+
+
+def test_encrypt_implementations_agree() -> None:
+    for length in [0, 1, 64, 65, 128, 1000]:
+        key = secrets.token_bytes(blake3.key_size)
+        nonce = secrets.token_bytes(length)[:MAX_NONCE_LEN]
+        message = secrets.token_bytes(length)
+        aad = secrets.token_bytes(length)
+        regular_result = encrypt(key, nonce, aad, message)
+        cli_result = encrypt_b3sum(key, nonce, aad, message)
         assert regular_result == cli_result, f"length {length}"
 
 
